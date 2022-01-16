3e59342b5d8af4001790316075ad10cae9bc4e67
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2022-01-16 23:11:45 +0100

    Trying to call external API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./server.js");
 const dbConnect = require("./db.js");
 const Subscriptions = require("./models/subscriptions");
-
+const { getLabels } = require("./externalAPI/imageLabeling");
 
 require("dotenv").config();
 
@@ -13,6 +13,19 @@ const subscriptions = new Subscriptions();
 subscriptions.initialize();
 subscriptions.execute();
 
+const labels = async() => {
+  let res;
+  try {
+    res = await getLabels("https://www.inferdo.com/img/label-1.jpg");
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+
+  return res;
+  
+}
+console.log(labels());
 
 dbConnect().then(
   () => {
